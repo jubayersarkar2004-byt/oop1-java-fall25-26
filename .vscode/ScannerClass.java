1c58b5641137c322681c2f6de43e37012250e4e4
jubayersarkar2004 <jubayersarkar2004@gmail.com>
2025-11-16 20:36:00 +0600

    25-62064-2

---
+++ .vscode/ScannerClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ScannerClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        byte age = scanner.nextByte();
+        System.out.print("Your age is: " + age);
+
+        String name = scanner.nextLine().trim();
+        System.out.print("Your name is: " + name);
+
+        scanner.close();
+    }
+}
+ 
---
+++ .vscode/TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int income = 120000;
+        String className = (income > 100000) ? "First" : "Economy";
+        System.out.println("Class: " + className);
+    }
+} 
