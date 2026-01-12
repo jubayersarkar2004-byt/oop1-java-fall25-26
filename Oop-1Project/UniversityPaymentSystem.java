ebdca554ea69dad6d65cd51583abc1971c54c5ed
jubayersarkar2004 <jubayersarkar2004@gmail.com>
2026-01-12 22:27:30 +0600

    25-62064-2

--- Oop-1Project/UniversityPaymentSystem.java
+++ Oop-1Project/UniversityPaymentSystem.java
@@ -62,12 +62,18 @@ class University {
 public class UniversityPaymentSystem {
     public static void main(String[] args) {
 
+        System.out.println("\n==>Payment<==");
+
         Student s1 = new UndergraduateStudent("Rahim", 101);
         Student s2 = new GraduateStudent("Karim", 201);
+        Student s3 = new UndergraduateStudent("Abdullah", 62064);
+        Student s4 = new GraduateStudent("Jubayer", 6262);
 
         University u = new University();
 
         u.processPayment(s1, (Payment) s1, 50000);
         u.processPayment(s2, (Payment) s2, 80000);
+        u.processPayment(s3, (Payment)s3, 70000);
+        u.processPayment(s3, (Payment)s4, 600000);
     }
 }
